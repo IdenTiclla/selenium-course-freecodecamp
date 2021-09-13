ee0b5851c2a774548702abc53332dad32628f5fb
IdenTiclla <iden.ticlla@gmail.com>
2021-09-13 16:39:38 -0400

    explicit vs implicit

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome(executable_path=r"C:\chromedriver\chromedriver.exe")
 
 driver.get("https://www.seleniumeasy.com/test/jquery-download-progress-bar-demo.html")
-driver.implicitly_wait(3)
+driver.implicitly_wait(8)
 my_element = driver.find_element_by_id("downloadButton")
 my_element.click()
 
+
+WebDriverWait(driver, 30).until(
+    EC.text_to_be_present_in_element(
+        (By.CLASS_NAME, "progress-label"), # Element filtration
+        "Completed!" # The expected text
+    )
+)
